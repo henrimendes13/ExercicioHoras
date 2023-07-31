ac90931f5f49d9f008a7e7d65b2389738edceb40
Henrique Mendes <henriquemendes13@gmail.com>
2023-07-30 21:23:08 -0300

    Add project files.

---
+++ ExercicioHoras/Program.cs
@@ -0,0 +1,12 @@
+﻿int n, horas, resto, minutos, segundos;
+
+Console.WriteLine("digite a quantidade de segundos: ");
+
+n = int.Parse(Console.ReadLine());
+horas = n / 3600;
+resto = n % 3600;
+minutos = resto / 60;
+segundos = resto % 60;
+
+Console.Write("Resultado em horas: " + horas + ":" + minutos + ":" + segundos);
+
